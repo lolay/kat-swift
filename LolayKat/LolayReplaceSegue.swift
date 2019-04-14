277f3696e30559a0e3b61d9f3a85dac7f94d0de7
Gary Rudolph <GaryRudolph@mac.com>
2019-04-14 10:47:19 -0700

    Added replace segue

---
+++ LolayKat/LolayReplaceSegue.swift
@@ -0,0 +1,47 @@
+//
+//  Copyright © 2019 Lolay, Inc.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import Foundation
+import UIKit
+
+/**
+ Instead of just pushing on top of the stack, this will replace the top controller with the destination controller.
+ This can only be applied to a Show in a navigation controller. Otherwise it will do a standard segue.
+ */
+class LolayReplaceSegue: UIStoryboardSegue {
+    /// The depth that this should replace. Default is to just replace the top one.
+    @IBInspectable var depth: Int = 1
+    
+    override func perform() {
+        if self.depth > 0, let navigationController = self.source.navigationController {
+            let count = navigationController.viewControllers.count
+            if self.depth > count {
+                navigationController.setViewControllers([self.destination], animated: true)
+            } else {
+                var viewControllers = navigationController.viewControllers
+                let start = count - self.depth
+                let end = count - 1
+                viewControllers.removeSubrange(start...end)
+                viewControllers.append(self.destination)
+                navigationController.setViewControllers(viewControllers, animated: true)
+            }
+            
+            // Do the default behavior
+        } else {
+            super.perform()
+        }
+    }
+}
