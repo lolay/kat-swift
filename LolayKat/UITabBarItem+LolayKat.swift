f12b63ce0cb52cd6422b85eb1ab85f7efe044d1d
Gary Rudolph <GaryRudolph@mac.com>
2019-04-16 23:05:44 -0700

    Added tab bar badge value.

---
+++ LolayKat/UITabBarItem+LolayKat.swift
@@ -0,0 +1,36 @@
+//
+//  Copyright © 2019 Lolay, Inc.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import Foundation
+import UIKit
+
+public extension UITabBarItem {
+    /*
+     Sets the badge value for a tab. I it exceeds `max` it sets it to "String(max) + \"+\"".
+     If it's `0` it sets it to an empty string.
+     - parameter value: The current value to set the badge to
+     - parameter max: The max after which to apend a "+" to the value
+     */
+    func setBadgeValue(_ value: Int, max: Int? = nil) {
+        if value <= 0 {
+            self.badgeValue = nil
+        } else if let max = max, value > max {
+            self.badgeValue = String(max) + "+"
+        } else {
+            self.badgeValue = String(value)
+        }
+    }
+}
---
+++ LolayKatTests/UITabBarItemTests.swift
@@ -0,0 +1,35 @@
+//
+//  Copyright © 2019 Lolay, Inc.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+import XCTest
+
+final class UITabBarItemTests: XCTestCase {
+    func testBadgeValue() {
+        let item = UITabBarItem(title: nil, image: nil, tag: 0)
+        XCTAssertNil(item.badgeValue)
+        
+        item.setBadgeValue(-1)
+        XCTAssertNil(item.badgeValue)
+        
+        item.setBadgeValue(50)
+        XCTAssertEqual(item.badgeValue, "50")
+        
+        item.setBadgeValue(100, max: 99)
+        XCTAssertEqual(item.badgeValue, "99+")
+        
+        item.setBadgeValue(99, max: 99)
+        XCTAssertEqual(item.badgeValue, "99")
+    }
+}
